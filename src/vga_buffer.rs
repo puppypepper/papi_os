0e8b4a6d8a0d73b4d8a645e9466d57cb5e9f6398
p6 <puppypepper21@gmail.com>
2024-10-23 01:00:57 +0900

    Use writer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -52,6 +52,14 @@ pub struct Writer {
 }
 
 impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
             b'\n' => self.new_line(),
@@ -75,4 +83,16 @@ impl Writer {
     fn new_line(&mut self) {
         // TODO
     }
+}
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
 }
