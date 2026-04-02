e92e11664798dbd31f8548e15debb3a5836fe984
papi <puppypepper21@gmail.com>
2026-04-03 01:03:50 +0900

    Add breakpoint page fault exception handler

---
+++ src/arch/x86_64/interrupts.rs
@@ -0,0 +1,39 @@
+use crate::arch::x86_64::hlt_loop;
+use crate::serial_println;
+use lazy_static::lazy_static;
+use x86_64::registers::control::Cr2;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
+
+lazy_static! {
+    pub static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+    serial_println!("IDT loaded");
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    serial_println!("EXCEPTION: BREAKPOINT");
+    serial_println!("{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    serial_println!("EXCEPTION: PAGE FAULT");
+    // Cr2 register is an x86_64's special register which contains the most recent page fault address.
+    serial_println!("Accessed Address: {:?}", Cr2::read());
+    serial_println!("Error Code: {:?}", error_code);
+    serial_println!("{:#?}", stack_frame);
+
+    hlt_loop();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+#![feature(abi_x86_interrupt)]
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-use core::panic::PanicInfo;
 use crate::arch::x86_64::hlt_loop;
+use crate::arch::x86_64::interrupts::init_idt;
+use core::arch::asm;
+use core::panic::PanicInfo;
 
+// this function is the entry point, since the linker looks for a function
+// named `_start` by default
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    init_idt();
+
     vga_buffer::print_something();
 
     serial_println!("kernel booted");
     serial_println!("hello from serial");
 
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    // `int3` instruction triggers breakpoint exception on x86_64.
+    unsafe {
+        asm!("int3");
+    }
 
     hlt_loop()
 }
