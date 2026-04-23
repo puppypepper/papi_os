191c3093578126fda9fe2595003f77b8143efb70
papi <puppypepper21@gmail.com>
2026-04-24 01:06:06 +0900

    Allocate usable frame

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use crate::arch::x86_64::interrupts::init_idt;
 use crate::arch::x86_64::pic::init_pics;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::structures::paging::FrameAllocator;
+use crate::memory::BootInfoFrameAllocator;
 
 entry_point!(kernel_main);
 
@@ -18,8 +20,16 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     memory::print_memory_map(&boot_info.memory_map);
 
+    let mut frame_allocator = BootInfoFrameAllocator::init(&boot_info.memory_map);
+
     // Enable hardware interrupts only after the GDT, IDT, and PIC are ready.
     x86_64::instructions::interrupts::enable();
+
+    for _ in 0..5 {
+        if let Some(frame) = frame_allocator.allocate_frame() {
+            serial_println!("allocated frame: {:#018x}", frame.start_address().as_u64());
+        }
+    }
 
     vga_buffer::print_something();
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,7 @@
 use crate::serial_println;
-use bootloader::bootinfo::MemoryMap;
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+use x86_64::PhysAddr;
+use x86_64::structures::paging::{FrameAllocator, PageSize, PhysFrame, Size4KiB};
 
 pub fn print_memory_map(memory_map: &MemoryMap) {
     serial_println!("=== START memory map information === ");
@@ -11,4 +13,35 @@ pub fn print_memory_map(memory_map: &MemoryMap) {
         serial_println!("{:#018x}..{:#018x} {:#?}", start, end, kind);
     }
     serial_println!("=== END memory map information === ");
+}
+
+fn usable_frame(memory_map: &'static MemoryMap) -> impl Iterator<Item = PhysFrame> {
+    memory_map
+        .iter()
+        .filter(|region| region.region_type == MemoryRegionType::Usable)
+        .flat_map(|region| region.range.start_frame_number..region.range.end_frame_number)
+        .map(|frame_number| frame_number * Size4KiB::SIZE)
+        .map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+}
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub fn init(memory_map: &'static MemoryMap) -> Self {
+        Self {
+            memory_map,
+            next: 0,
+        }
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = usable_frame(self.memory_map).nth(self.next);
+        self.next += 1;
+        frame
+    }
 }
