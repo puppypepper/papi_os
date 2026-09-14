ed215c1a3e36e225d83fc817dd421d11c2943918
papi <puppypepper21@gmail.com>
2026-09-15 02:09:14 +0900

    Temporarily allow deadcode

--- src/task.rs
+++ src/task.rs
@@ -1,11 +1,13 @@
 use alloc::boxed::Box;
 use core::future::Future;
 
+#[allow(dead_code)]
 struct Task {
     future: Box<dyn Future<Output = ()>>,
 }
 
 impl Task {
+    #[allow(dead_code)]
     fn new(future: impl Future<Output = ()> + 'static) -> Self {
         Task {
             future: Box::new(future),
